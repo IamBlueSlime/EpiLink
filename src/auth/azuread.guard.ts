3fe9ae29334694e843025192079387b741db9212
Jérémy Levilain <jeremy.levilain@epitech.eu>
2021-04-19 12:41:57 +0200

    feat: add debug logs

--- src/auth/azuread.guard.ts
+++ src/auth/azuread.guard.ts
@@ -4,7 +4,9 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class AzureAdAuthGuard extends AuthGuard('azuread') {
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log('guard');
     const can = await super.canActivate(context);
+    console.log('guard can', can);
 
     if (can) {
       const request = context.switchToHttp().getRequest();
