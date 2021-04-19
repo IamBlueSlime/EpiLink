d25b0ae03eba53d38bb8ee6cab97fd18516da50c
Jérémy Levilain <jeremy.levilain@epitech.eu>
2021-04-19 14:11:31 +0200

    feat: debug session

--- src/auth/azuread.guard.ts
+++ src/auth/azuread.guard.ts
@@ -1,3 +1,20 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-export const AzureAdAuthGuard = AuthGuard('azuread');
+@Injectable()
+export class AzureAdAuthGuard extends AuthGuard('azuread') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log('guard');
+    const request = context.switchToHttp().getRequest();
+    console.log(request.session);
+    const can = await super.canActivate(context);
+    console.log('guard can', can);
+
+    if (can) {
+      const request = context.switchToHttp().getRequest();
+      void super.logIn(request);
+    }
+
+    return true;
+  }
+}
